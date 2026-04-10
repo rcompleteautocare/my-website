fd30d25037df31318e4bc03e2ae1e8bc9c045703
R Complete Auto Care <Owner@rcompleteautocare.com>
2026-04-10 10:53:11 -0500

    Add next-sitemap config, robots.txt, and updated sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.rcompleteautocare.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.8,
+}
