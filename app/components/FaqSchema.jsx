aea385e29a2c9d3d9c620e656e16cbb3c908499b
R Complete Auto Care <Owner@rcompleteautocare.com>
2026-04-14 01:37:18 -0500

    restore FaqSchema component

---
+++ app/components/FaqSchema.jsx
@@ -0,0 +1,17 @@
+export default function FaqSchema({ faqs }) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    "mainEntity": faqs.map(({ q, a }) => ({
+      "@type": "Question",
+      "name": q,
+      "acceptedAnswer": { "@type": "Answer", "text": a }
+    }))
+  }
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+    />
+  )
+}
